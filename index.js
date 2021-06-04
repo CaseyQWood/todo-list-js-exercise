82e786215549bd223abba828fa286002d5504796
Casey Wood <caseywood252@gmail.com>
2021-06-04 20:19:38 +0000

    linting

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
+
 function newTask(title, descriptions) {
   const task ={
     title: title,
@@ -13,21 +12,11 @@ function newTask(title, descriptions) {
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     }
   }
-  return task
+  return task;
 }
 
+task1.logState();
+task1.markCompleteTask();
+task1.logState();
 
-
-// newTask("Clean Cat Litter"); // task 0
-// newTask("Do Laundry"); // task 1
-
-task1.logState()
-task1.markCompleteTask()
-task1.logState()
-
-
-// newTask(task1); // Clean Cat Litter has not been completed
-// newTask(task1);
-// newTask(task1); // Clean Cat Litter has been completed
-
-console.log(tasks)
+console.log(tasks);
